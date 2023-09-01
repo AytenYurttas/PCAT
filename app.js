aad889d9222c0303fb39834ceb51d4c390233f27
AytenYurttas <aytenyurttas1107@gmail.com>
2023-09-01 17:57:51 +0300

    template engine

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const ejs =require('ejs');
+
 const app = express();
 
+//TEMPLATE ENGİNE
+app.set("view engine","ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'))
 
-
+//ROUTES
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    // res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render("index")
 });
+app.get('/about',(req,res)=>{
+    res.render("about")
+});
+app.get('/add',(req,res)=>{
+    res.render("add")
+});
+
 
 const port =3000;
 app.listen(port, ()=>{
