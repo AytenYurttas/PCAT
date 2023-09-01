f9801cc856d46b1f97bda418f51bc1a120717637
AytenYurttas <aytenyurttas1107@gmail.com>
2023-09-01 16:44:59 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const expresss =require('express')
+const express = require('express')
+const path = require('path')
+const app = express();
 
-const app =expresss();
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+
 app.get('/',(req,res)=>{
-    res.send('merhaba')
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
 });
+
 const port =3000;
 app.listen(port, ()=>{
     console.log(`sunucu ${port} portunda başlatıldı..`)
