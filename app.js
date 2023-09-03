a3c1f165fa61c6ed8f8ead721f39a79a8fe2d9d5
AytenYurttas <aytenyurttas1107@gmail.com>
2023-09-03 21:08:42 +0300

    file-upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //NODE MODULES
 const express = require('express');
 const mongoose= require('mongoose');
+const fileUpload = require('express-fileupload'); 
 const path = require('path');
 const ejs =require('ejs');
+const fs = require('fs');
 const Photo =require('./models/Photo');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -23,13 +25,14 @@ mongoose.connect(process.env.DB_URL)
 app.set("view engine","ejs");
 
 //MIDDLEWARES
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get('/',async (req,res)=>{
-    const photos = await Photo.find({})
+    const photos = await Photo.find({}).sort('-uploadeAt');
     res.render("index",{
       photos
     })
@@ -51,8 +54,27 @@ app.get('/add',(req,res)=>{
 });
 
 app.post('/photos',async (req,res)=>{
-  await Photo.create(req.body)
+
+  // console.log(req.files.image);
+  // await Photo.create(req.body)
+  // res.redirect('/');
+  
+  const  uploadeDir = 'public/uploads';
+
+  if(!fs.existsSync(uploadeDir)){
+    fs.mkdirSync(uploadeDir)
+  }
+  
+  let uploadeImage = req.files.image
+  let uploadPath =  __dirname + '/public/uploads/' + uploadeImage.name;
+  
+  uploadeImage.mv(uploadPath, async ()=> {
+  await Photo.create({
+    ...req.body,
+    image:'/uploads/' + uploadeImage.name
+  });
   res.redirect('/');
+  });
 });
 
 
